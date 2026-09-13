8b98f07c3c47658397624552fa449f53189d56a2
AlexMurev <alemurovalex@yandex.ru>
2026-09-13 21:02:40 +0500

    add autoupdate

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,56 @@
-import { app, shell, BrowserWindow, ipcMain } from 'electron';
+import { app, shell, BrowserWindow, ipcMain, dialog } from 'electron';
 import { join } from 'path';
 import { electronApp, optimizer, is } from '@electron-toolkit/utils';
+import { autoUpdater } from 'electron-updater';
 import icon from '../../resources/icon.png?asset';
 import iconIco from '../../resources/icon.ico?asset';
 import { WebSocketServer } from 'ws';
+import log from 'electron-log';
+
+// Настройка логирования автообновлений (логи сохраняются в %USERPROFILE%\AppData\Roaming\golub\logs)
+autoUpdater.logger = log;
+log.transports.file.level = 'info';
 
 const windowIcon = process.platform === 'win32' ? iconIco : icon;
+
+// Функция для управления логикой автообновления
+function checkUpdates(): void {
+  // Не запускаем автообновление в режиме разработки, чтобы избежать лишних ошибок в консоли
+  if (is.dev) return;
+
+  // Автоматически проверяет обновления и скачивает их в фоне
+  autoUpdater.checkForUpdatesAndNotify();
+
+  // Событие: Обнаружено обновление на GitHub, начинается фоновое скачивание
+  autoUpdater.on('update-available', () => {
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Доступно обновление',
+      message: 'Обнаружена новая версия приложения. Она уже скачивается в фоновом режиме.',
+      buttons: ['Ок']
+    });
+  });
+
+  // Событие: Файлы скачаны и готовы к установке
+  autoUpdater.on('update-downloaded', () => {
+    dialog
+      .showMessageBox({
+        type: 'question',
+        buttons: ['Установить и перезапустить', 'Позже'],
+        defaultId: 0,
+        title: 'Обновление готово к установке',
+        message:
+          'Новая версия успешно скачана. Перезапустить приложение сейчас, чтобы применить изменения?'
+      })
+      .then((result) => {
+        // Если пользователь нажал первую кнопку ("Установить и перезапустить")
+        if (result.response === 0) {
+          // closeBeforeQuit = false, restart = true
+          autoUpdater.quitAndInstall(false, true);
+        }
+      });
+  });
+}
 
 function createWindow(): void {
   const mainWindow = new BrowserWindow({
@@ -109,6 +154,9 @@ app.whenReady().then(() => {
   ipcMain.on('ping', () => console.log('pong'));
 
   createWindow();
+
+  // Запуск проверки обновлений (будет работать только в прод-сборке благодаря проверке !is.dev)
+  checkUpdates();
 
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
